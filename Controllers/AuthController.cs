572628472dc5100902a8c55f7770e79197954263
caesar <joaovictorcsatu@gmail.com>
2022-05-24 09:53:44 -0300

    Added more functionality to user autentication

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,7 +1,9 @@
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using TechDistributor.Data;
 using TechDistributor.Models;
 
@@ -12,12 +14,14 @@ namespace TechDistributor.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly AppDbContext _context;
-
+    
     public AuthController(AppDbContext context)
     {
         _context = context;
     }
-    
+
+    private readonly IConfiguration _config;
+
     [HttpPost("api/register")]
     public async Task<ActionResult<User>> UserRegister(User obj)
     {
@@ -30,18 +34,40 @@ public class AuthController : ControllerBase
     [AllowAnonymous]
     public async Task<ActionResult<UserLogin>> UserLogin([FromBody]UserLogin obj)
     {
-         
+        var user = Authenticate(obj);
+
+        if (user != null)
+        {
+            var token = Generate(user);
+            return Ok(token);
+        }
         
         return Ok();
     }
 
-    public Task<User?> Authenticate(UserLogin obj)
+    private User Authenticate(UserLogin obj)
     {
-        var currentUser = _context.users.FirstOrDefaultAsync(o => o.Username.ToLower() == obj.username.ToLower() 
-                                                                   && o.Password == obj.password);
-        
-        if (currentUser == null) return null;
+        var currentUser = _context.users.FirstOrDefault(o => o.Username.ToLower() == obj.username.ToLower() 
+                                                                  && o.Password == obj.password);
 
-        return currentUser;
+        if (currentUser != null)
+        {
+            return currentUser;
+        }
+
+        return null;
+    }
+
+    private string Generate(User obj)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, obj.Username)
+        };
+        
+        return "";
     }
 }
